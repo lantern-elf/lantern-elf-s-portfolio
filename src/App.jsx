4e8fcdeecc671828e6e723c4bcca03c6b2164135
Lantern Elf <lantern4k@gmail.com>
2024-11-24 18:45:42 +0700

    Adding routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { useState } from 'react'
+import Home from './pages/home/Home'
+import NotFound from './pages/not_found/NotFound'
 import './App.css'
 
-function App() {
+const App = () => {
   return (
-    <>
-    </>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='*' element={<NotFound/>} />
+      </Routes>
+    </Router>
   )
 }
 
