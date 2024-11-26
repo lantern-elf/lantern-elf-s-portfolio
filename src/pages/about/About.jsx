914be101eeda417e527bbfdbeee6b9145b4f34c4
Lantern Elf <lantern4k@gmail.com>
2024-11-26 19:59:45 +0700

    updating about

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,4 +1,6 @@
 import { Helmet } from "react-helmet";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTwitter, faInstagram, faFacebook, faBluesky, faGithub } from '@fortawesome/free-brands-svg-icons'
 import Navbar from "../../components/navbar/Navbar";
 import './about.css'
 
@@ -26,12 +28,17 @@ const About = () => {
                     </div>
                     <div className="about-info">
                         <p>
-                            Greetings. I am Lantern, a 2D Artist and programmer, based on Indonesia.
-                            I specialized in art and coding. My expertise includes illustrations, concept art, 
+                            Greetings. I am <b>Lantern</b>. I am a <i>2D Artist</i> and a <i>programmer</i>, based on Indonesia.
+                            I specialized in <b>art</b> and <b>coding</b>. My expertise includes illustrations, concept art, 
                             front-end website development, and game development.
                         </p>
                     </div>
                     <div className="about-links">
+                        <a href="" target="black"><FontAwesomeIcon icon={faTwitter} /></a>
+                        <a href="" target="black"><FontAwesomeIcon icon={faInstagram} /></a>
+                        <a href="" target="black"><FontAwesomeIcon icon={faBluesky} /></a>
+                        <a href="" target="black"><FontAwesomeIcon icon={faFacebook} /></a>
+                        <a href="" target="black"><FontAwesomeIcon icon={faGithub} /></a>
                     </div>
                 </div>
             </div>
