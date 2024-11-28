d862c73c66ecf1f3fcf36cd1fe3d9696efeb2483
Lantern Elf <lantern4k@gmail.com>
2024-11-28 15:14:04 +0700

    Updating artworks page

---
+++ src/components/content_card/ContentCard.jsx
@@ -0,0 +1,10 @@
+import "./contentCard.css"
+
+const ContentCard = () =>{
+    return(
+        <div className="container card">
+        </div>
+    )
+}
+
+export default ContentCard
--- src/pages/artworks/Artworks.jsx
+++ src/pages/artworks/Artworks.jsx
@@ -1,12 +1,21 @@
 import { Helmet } from "react-helmet";
+import Masonry from "react-masonry-css";
 import Navbar from "../../components/navbar/Navbar";
 import './artworks.css'
+import artworkData from "../../data/artworks.json";
 
 const Artworks = () => {
+    const breakpointColumnsObj = {
+        default: 4,
+        1100: 3,
+        700: 3,
+        500: 2
+    }
+
     return(
         <>
             <Helmet>
-                <title>{`Lantern Elf | Arts`}</title>
+                <title>{`Lantern Elf | Artworks`}</title>
             </Helmet>
             <Navbar
                 menuState1={false}
@@ -14,6 +23,22 @@ const Artworks = () => {
                 menuState3={false}
                 menuState4={false}
             />
+            <div className="gallery-wrapper">
+                <Masonry
+                    breakpointCols={breakpointColumnsObj}
+                    className="my-masonry-grid"
+                    columnClassName="my-masonry-grid_collumn"
+                >
+                    {
+                        artworkData.map((image) => (
+                            <div key={image.id} className="pin">
+                                <img className="artwork-img" src={image.url} />
+                                {console.log(image.id)}
+                            </div>
+                        ))
+                    }
+                </Masonry>
+            </div>
         </>
     )
 }
